fb8a48ebf9da328e5b049394356a20aa0e28f8f3
Ian Scateni <ianscateni@hotmail.com>
2025-06-14 23:11:01 +0000

    feat: configuración inicial de Java en LXC Fedora y primera ejecución exitosa

---
+++ backend/java/laboratoryOne/MiPrimerPrograma.java
@@ -0,0 +1,13 @@
+public class MiPrimerPrograma {
+  public static void main (String[] args) {
+      
+    System.out.println("I’m a Programmer");
+
+    System.out.println("Let’s change the code now…");
+
+    System.out.println("I’m a Programmer, and I love coding!");
+
+    System.out.println("it’s what I do!");
+      
+  }
+}
