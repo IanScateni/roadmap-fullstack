f0ccafad5e67d402db826e6c9d07665897926ab2
Ian Scateni <ianscateni@hotmail.com>
2025-06-16 01:03:06 +0000

    Winston NodeJS

---
+++ backend/nodejs/02-bases/src/plugins/logger.plugin.js
@@ -0,0 +1,24 @@
+const winston = require( 'winston' );
+
+const logger = winston.createLogger( {
+
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [ 
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ], 
+
+} );
+
+
+module.exports = function buildLogger(service) {
+  
+  return {
+    info: (message) => {
+      logger.log( 'info', message, service);
+    }
+  }
+
+}
