d393e16f995ea4ffd45949ed108b7331aaea1ec5
Ian Scateni <ianscateni@hotmail.com>
2025-06-24 19:07:57 +0000

    Update Java

---
+++ backend/java/basic/conditionals/Logic.java
@@ -0,0 +1,13 @@
+public class Logic {
+   public static void main(String[] args) {
+       //marks between 1 and 100
+       int marks = 75;
+
+       if(marks > 60) {
+           System.out.println("Pass.");
+       } else {
+           System.out.println("Fail.");
+       }
+
+   }
+}
