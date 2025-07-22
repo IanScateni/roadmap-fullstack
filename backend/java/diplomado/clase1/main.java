ba0f26f0a1968b7d3b8175640b139a84ee7dfc9b
Ian Scateni <ianscateni@hotmail.com>
2025-07-22 00:06:54 +0000

    Add java class 1 diplomado

---
+++ backend/java/diplomado/clase1/main.java
@@ -0,0 +1,20 @@
+public class main {
+
+  public static void main(String[] args) {
+
+    String saludar = "Hola";
+    System.out.println(saludar);
+    System.out.println(saludar.toUpperCase());
+
+
+    byte numberoByte = 127;
+    System.out.println("numeroByte = " + numberoByte);
+    
+    System.out.println("Tipo byte corresponde en byte a " + Byte.BYTES);
+    System.out.println("Tipo byte corresponde en byte a " + Byte.SIZE);
+
+ 
+
+  }
+
+}
